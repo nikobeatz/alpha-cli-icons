239577c4d84e24c0835a16b0bd1f2524290101c4
Automated Publisher <actions@users.noreply.github.com>
2021-03-04 07:36:55 +0000

    Automated publish: Thu Mar  4 07:36:55 UTC 2021 7493b00166caa42d96cbd41d617b7929ed10816e

---
+++ components/CircleS.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+export const CircleS = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={32}
+    height={32}
+    fill="none"
+    viewBox="0 0 32 32"
+    {...props}
+  >
+    <path d="M0 0h24v24H0z" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 3a9 9 0 100 18 9 9 0 000-18zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"
+      fill="currentColor"
+    />
+  </svg>
+);
