0fd78a13c13e45b305697e63cf3db5db55c4b765
Automated Publisher <actions@users.noreply.github.com>
2021-03-06 11:07:57 +0000

    Automated publish: Sat Mar  6 11:07:57 UTC 2021 69ee4c1c9be0e6b7315c42e4d2ca85675328af62

--- publish/index.js
+++ publish/index.js
@@ -4,9 +4,33 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var React = require('react');
 
+const BookS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
+    React.createElement("path", { d: "M0 0h24v24H0z" }),
+    React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M21.185 3.662v12.926c0 .474-.386.86-.86.862-1.999.005-5.348.423-7.659 2.85V6.772c0-.16.041-.311.118-.436 1.897-3.066 5.541-3.531 7.544-3.536h.003a.86.86 0 01.854.862zM3.669 2.8h.002c2.004.005 5.648.47 7.544 3.536a.828.828 0 01.119.437V20.3c-2.311-2.427-5.66-2.845-7.66-2.85a.863.863 0 01-.86-.862V3.662A.86.86 0 013.67 2.8zm19.472 2.98h-.622v10.808c0 1.21-.983 2.197-2.19 2.2-1.696.004-4.492.337-6.472 2.218 3.425-.842 7.035-.295 9.092.176a.858.858 0 001.05-.84v-13.7a.861.861 0 00-.858-.861zm-21.66 0v10.808c0 1.21.983 2.197 2.19 2.2 1.696.004 4.492.337 6.472 2.218-3.425-.842-7.035-.295-9.092.176a.858.858 0 01-1.05-.84v-13.7c-.001-.475.384-.861.858-.861h.622z", fill: "currentColor" })));
+
+const CheckmarkRoudnedS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
+    React.createElement("path", { d: "M0 0h24v24H0z" }),
+    React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M0 12C0 5.373 5.373 0 12 0s12 5.373 12 12-5.373 12-12 12S0 18.627 0 12zm17.76-2.967a.818.818 0 00-1.157-1.157l-6.512 6.512-2.694-2.694a.818.818 0 00-1.157 1.157l3.272 3.273c.32.32.838.32 1.157 0l7.091-7.09z", fill: "currentColor" })));
+
 const CircleS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
     React.createElement("path", { d: "M0 0h24v24H0z" }),
     React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M12 8.727a3.273 3.273 0 100 6.546 3.273 3.273 0 000-6.546zM8 12a4 4 0 118 0 4 4 0 01-8 0z", fill: "currentColor" })));
+
+const ClockS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
+    React.createElement("path", { d: "M0 0h24v24H0z" }),
+    React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M16.472 1.703a4.107 4.107 0 015.807 5.807c-.32.32-.84.32-1.161 0l-4.646-4.646a.821.821 0 010-1.161zm-8.98 0A4.107 4.107 0 001.685 7.51c.321.32.841.32 1.162 0l4.647-4.646c.32-.32.32-.84 0-1.162zm11.014 17.353a9.036 9.036 0 10-13.048 0l-2.348 3.13a.821.821 0 001.315.985L6.7 20.137a8.97 8.97 0 0010.564 0l2.276 3.034a.821.821 0 101.314-.985l-2.348-3.13zm-5.702-6.235c0 .178-.058.35-.165.493L10.175 16.6a.821.821 0 11-1.314-.986l2.3-3.067V7.892a.821.821 0 011.643 0v4.93z", fill: "currentColor" })));
+
+const HomeS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
+    React.createElement("path", { d: "M0 0h24v24H0z" }),
+    React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M11.659 2.145a.21.21 0 01.244-.002l10.443 7.276a1.053 1.053 0 001.203-1.728L13.107.416a2.316 2.316 0 00-2.686.027L.434 7.703a1.053 1.053 0 001.238 1.703l9.987-7.26zm-7.811 9.309a2 2 0 01.805-1.604l5.953-4.431a2 2 0 012.345-.032l6.351 4.465a2 2 0 01.85 1.636v9.593a2 2 0 01-2 2H5.848a2 2 0 01-2-2v-9.627zm8.994 2.154a1.895 1.895 0 113.79 0 1.895 1.895 0 01-3.79 0z", fill: "currentColor" })));
+
+const LinkS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
+    React.createElement("path", { d: "M0 0h24v24H0z" }),
+    React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M11.235 5H5.353a3 3 0 00-3 3v2.706a3 3 0 003 3h2.412v-.118h2v.118h1.47a3 3 0 003-3v-.412h-1.47c-.971 0-1.834.461-2.383 1.177H8.108a5.002 5.002 0 014.657-3.177h5.882a5 5 0 015 5V16a5 5 0 01-5 5h-5.882a5 5 0 01-5-5v-.294H5.353a5 5 0 01-5-5V8a5 5 0 015-5h5.882a5.002 5.002 0 014.657 3.176h-2.274A2.995 2.995 0 0011.235 5zm-1.47 10.706V16a3 3 0 003 3h5.882a3 3 0 003-3v-2.706a3 3 0 00-3-3h-2.412v.412a5 5 0 01-5 5h-1.47z", fill: "currentColor" })));
+
+const LoudspeakerS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
+    React.createElement("path", { d: "M0 0h24v24H0z" }),
+    React.createElement("path", { d: "M19.573 13.73a23.96 23.96 0 00-10.258 2.779l-3.988 2.384c-1.054.63-2.401.252-3.006-.834L.294 14.424c-.606-1.088-.245-2.486.806-3.114l3.304-1.976a.393.393 0 01.546.152l2.596 4.657a.392.392 0 00.557.145c.187-.119.238-.378.128-.574L5.616 9.023a.423.423 0 01.118-.547 25.133 25.133 0 006.807-7.362 1.645 1.645 0 012.867.058l5.55 9.957c.63 1.129-.125 2.55-1.385 2.6zm.712-6.936c.205-1.218-.484-2.44-1.598-2.857-.35-.132-.668.263-.482.597l1.338 2.4c.188.337.677.244.742-.14zm-9.234 14.66l-1.898-3.413a.393.393 0 00-.547-.151c-1.029.614-1.068.638-2.44 1.459a.423.423 0 00-.147.566l1.902 3.41c.244.438.682.675 1.128.675.433 0 .6-.152 1.526-.706.621-.371.835-1.197.476-1.84zm12.155-17.81a.422.422 0 00.146-.566.393.393 0 00-.547-.152l-1.034.62c-.355.211-.207.773.2.773.142 0 .114-.005 1.235-.675zm-3.679-2.147l.253-.975a.416.416 0 00-.283-.508.399.399 0 00-.49.293l-.252.976c-.068.262.123.521.386.521a.403.403 0 00.386-.307zm4.46 6.571a.416.416 0 00-.284-.507l-.941-.262a.399.399 0 00-.49.293.416.416 0 00.283.508c1.03.286.973.275 1.045.275a.403.403 0 00.386-.307z", fill: "currentColor" })));
 
 const PresentS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
     React.createElement("path", { d: "M0 0h24v24H0z" }),
@@ -16,6 +40,12 @@ const WifiM = (props) => (React.createElement("svg", Object.assign({ xmlns: "htt
     React.createElement("path", { d: "M0 0h24v24H0z" }),
     React.createElement("path", { d: "M.487 9.538c.59.64 1.528.703 2.165.125C8.06 4.84 15.91 4.84 21.33 9.651a1.54 1.54 0 002.189-.113c.683-.741.637-1.972-.116-2.638-6.612-5.867-16.176-5.867-22.8 0-.752.653-.81 1.884-.115 2.638zm8.985 9.748l1.702 1.847a1.09 1.09 0 001.633 0l1.702-1.847c.544-.59.429-1.608-.266-1.997a4.614 4.614 0 00-4.528 0c-.66.389-.787 1.406-.243 1.997zM5.153 14.6c.568.616 1.46.679 2.12.164 2.825-2.174 6.623-2.174 9.448 0 .66.502 1.551.452 2.119-.164l.011-.012c.695-.754.649-2.035-.15-2.65-3.983-3.129-9.414-3.129-13.409 0-.799.627-.845 1.896-.139 2.662z", fill: "currentColor" })));
 
+exports.BookS = BookS;
+exports.CheckmarkRoudnedS = CheckmarkRoudnedS;
 exports.CircleS = CircleS;
+exports.ClockS = ClockS;
+exports.HomeS = HomeS;
+exports.LinkS = LinkS;
+exports.LoudspeakerS = LoudspeakerS;
 exports.PresentS = PresentS;
 exports.WifiM = WifiM;
