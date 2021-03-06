95d4b155d72337466132cf637e005cb891166c33
Automated Publisher <actions@users.noreply.github.com>
2021-03-06 10:59:35 +0000

    Automated publish: Sat Mar  6 10:59:35 UTC 2021 57d0199581ee0e180257dbc8a1545ada1f587147

---
+++ publish/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+const CircleS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
+    React.createElement("path", { d: "M0 0h24v24H0z" }),
+    React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M12 8.727a3.273 3.273 0 100 6.546 3.273 3.273 0 000-6.546zM8 12a4 4 0 118 0 4 4 0 01-8 0z", fill: "currentColor" })));
+
+const PresentS = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
+    React.createElement("path", { d: "M0 0h24v24H0z" }),
+    React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M18.567 4.547h1.857c1.026 0 1.862.857 1.862 1.91v2.836c0 1.053-.836 1.91-1.862 1.91h-.435v4.969h-7.271v-4.969h-1.436v4.969H4.011v-4.969h-.435c-1.026 0-1.862-.857-1.862-1.91V6.458c0-1.054.836-1.91 1.862-1.91H5.55A3.336 3.336 0 016.326.974a3.445 3.445 0 014.807.003l.89.956.97-.957c2.122-2.079 5.8-.625 5.8 2.352 0 .42-.079.834-.226 1.218zM16.784 1.97a1.99 1.99 0 00-2.774 0l-1.292 1.274v1.303h4.23c.574-.748.569-1.86-.164-2.577zm-6.68-.01a1.99 1.99 0 00-2.763.01 1.955 1.955 0 00-.167 2.577h4.108v-1.32l-1.178-1.268zM4.01 22.091c0 1.052.874 1.908 1.947 1.908h5.325v-6.422H4.01v4.514zM12.718 24h5.325c1.073 0 1.947-.856 1.947-1.908v-4.514h-7.272V24z", fill: "currentColor" })));
+
+const WifiM = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", width: 32, height: 32, fill: "none", viewBox: "0 0 32 32" }, props),
+    React.createElement("path", { d: "M0 0h24v24H0z" }),
+    React.createElement("path", { d: "M.487 9.538c.59.64 1.528.703 2.165.125C8.06 4.84 15.91 4.84 21.33 9.651a1.54 1.54 0 002.189-.113c.683-.741.637-1.972-.116-2.638-6.612-5.867-16.176-5.867-22.8 0-.752.653-.81 1.884-.115 2.638zm8.985 9.748l1.702 1.847a1.09 1.09 0 001.633 0l1.702-1.847c.544-.59.429-1.608-.266-1.997a4.614 4.614 0 00-4.528 0c-.66.389-.787 1.406-.243 1.997zM5.153 14.6c.568.616 1.46.679 2.12.164 2.825-2.174 6.623-2.174 9.448 0 .66.502 1.551.452 2.119-.164l.011-.012c.695-.754.649-2.035-.15-2.65-3.983-3.129-9.414-3.129-13.409 0-.799.627-.845 1.896-.139 2.662z", fill: "currentColor" })));
+
+exports.CircleS = CircleS;
+exports.PresentS = PresentS;
+exports.WifiM = WifiM;
